8cfb00205e77f7651a99eb6210343b3951c1e433
krboelter <krboelter@gmail.com>
2019-07-12 19:49:47 -0600

    Finished, stretches and all

--- challenges/classes.js
+++ challenges/classes.js
@@ -22,11 +22,34 @@ class CuboidMaker {
     }
 }
 
-const cuboid = new CuboidMaker({
+class CubeMaker extends CuboidMaker {
+    constructor(attribute) {
+        super(attribute);
+    }
+
+    cubeVolume() {
+        return this.long * this.width * this.height;
+    }
+
+    cubeSurfaceArea() {
+        return 2 * (this.long * this.width + this.long * this.height + this.width * this.height);
+    }
+}
+
+const cuboid = new CuboidMaker ({
     long: 4,
     width: 5,
     height: 5
 });
 
+const cube = new CubeMaker ({
+    long: 3,
+    width: 6,
+    height: 9
+});
+
 console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
+
+console.log(cube.volume());
+console.log(cube.surfaceArea());
